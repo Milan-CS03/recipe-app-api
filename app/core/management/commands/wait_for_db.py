bcc906727800a92a12697ed80203dd82ab6539f7
milan <milan.mech.iitb@gmail.com>
2024-02-11 15:36:59 +0530

    na

---
+++ app/core/management/commands/wait_for_db.py
@@ -0,0 +1,12 @@
+"""
+django command to eliminate race condition
+"""
+
+from typing import Any
+from django.core.management.base import BaseCommand
+
+class Command(BaseCommand):
+
+    def handle(self, *args: Any, **options: Any):
+        pass
+        return super().handle(*args, **options)
